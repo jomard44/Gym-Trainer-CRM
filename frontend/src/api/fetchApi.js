b3ee07ecfb6edb2673ba6f9bb778acbf0ba69af0
jomard <jomrdali44@gmail.com>
2026-02-20 14:41:05 -0400

    createContext for auth

--- frontend/src/api/fetchApi.js
+++ frontend/src/api/fetchApi.js
@@ -1,7 +1,21 @@
-const fetchApi = () => {
-  const res = fetch(import.meta.env.API, {
-    credentials: "included",
-  });
+const fetchApi = async (endPoint,options = {}) => {
+  try {
+    const res = await fetch(`${import.meta.env.VITE_API}${endPoint}`, {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      ...options
+    });
+
+    if (!res.ok) {
+      return null;
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return null;
+  }
 };
 
 export default fetchApi;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express"
 import { register, signin } from "../controllers/userController.js"
+import { auth } from "../middleware/auth.js"
 
 const userRouter = express.Router()
 
 userRouter.post("/register",register)
 userRouter.post("/signin",signin)
+userRouter.get("/me",auth,(req,res)=>{
+    res.json(req.user)
+})
 
 export default userRouter
