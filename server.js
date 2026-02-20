91467e000ee27213aad47b535c202d78bc6c9d77
jomard <jomrdali44@gmail.com>
2026-02-20 14:44:35 -0400

    install and config cors

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 import router from "./routes/index.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
